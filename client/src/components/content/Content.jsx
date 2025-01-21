6601200222c62040897fab48357c2ed08b8a1594
jugendrakashyap <kjugendra63@gmail.com>
2025-01-21 19:23:44 +0530

    fix scrolling functionality

--- client/src/components/content/Content.jsx
+++ client/src/components/content/Content.jsx
@@ -3,8 +3,34 @@ import React from 'react'
 import '../../css/content.css'
 
 function content() {
-    function scrollPage() {
-        document.getElementById('scrollable').scrollBy({ left: 500, behavior: 'smooth' })
+    function scrollRight() {
+        const container = document.getElementById('scrollable')
+        container.scrollBy({ left: 500, behavior: 'smooth' })
+    }
+
+    function scrollLeft() {
+        const container = document.getElementById('scrollable');
+        container.scrollBy({ left: -500, behavior: 'smooth' });
+    }
+    
+    function checkScrollEnd() {
+        const container = document.getElementById('scrollable');
+        const scrollLeftBtn = document.getElementById('scroll_left_btn');
+        const scrollRightBtn = document.getElementById('scroll_right_btn');
+        console.log(container.scrollLeft);
+
+        if (container.scrollLeft === 0) {
+            scrollLeftBtn.style.display = 'none';
+        } else {
+            scrollLeftBtn.style.display = 'block';
+        }
+        
+        if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+            scrollRightBtn.style.display = 'none';
+            scrollLeftBtn.style.display = 'block';
+        } else {
+            scrollRightBtn.style.display = 'block';
+        }
     }
     return (
         <main>
@@ -28,8 +54,46 @@ function content() {
                     </div>
                     <div className="forStyle"></div>
                 </div>
+                <div className="books" id='scrollable' onScroll={checkScrollEnd}>
+                    <div className='scroll_btn left_scroll_btn' id='scroll_left_btn' onClick={scrollLeft}></div>
+                    <div>
+                        <div className="book book1" onClick={() => window.location.href = "/book"}></div>
+                    </div>
+                    <div>
+                        <div className="book book2" onClick={() => window.location.href = "/book"}></div>
+                    </div>
+                    <div>
+                        <div className="book book3" onClick={() => window.location.href = "/book"}></div>
+                    </div>
+                    <div>
+                        <div className="book book4" onClick={() => window.location.href = "/book"}></div>
+                    </div>
+                    <div>
+                        <div className="book book1" onClick={() => window.location.href = "/book"}></div>
+                    </div>
+                    <div>
+                        <div className="book book2" onClick={() => window.location.href = "/book"}></div>
+                    </div>
+                    <div>
+                        <div className="book book3" onClick={() => window.location.href = "/book"}></div>
+                    </div>
+                    <div>
+                        <div className="book book4" onClick={() => window.location.href = "/book"}></div>
+                    </div>
+                    <div className='scroll_btn right_scroll_btn' id='scroll_right_btn' onClick={scrollRight}></div>
+                </div>
+            </section>
+
+            <section className="sec sec1">
+                <div className="label1">
+                    <div className="label">
+                        <h1>Discover Our Latest Releases</h1>
+                        <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis ipsum sed felis</p>
+                    </div>
+                    <div className="forStyle"></div>
+                </div>
                 <div className="books" id='scrollable'>
-                    <div className='scroll_btn' onClick={scrollPage}></div>
+                    <div className='scroll_btn' onClick={scrollLeft}></div>
                     <div>
                         <div className="book book1" onClick={() => window.location.href = "/book"}></div>
                     </div>
@@ -66,7 +130,7 @@ function content() {
                     <div className="forStyle"></div>
                 </div>
                 <div className="books" id='scrollable'>
-                    <div className='scroll_btn' onClick={scrollPage}></div>
+                    <div className='scroll_btn' onClick={scrollLeft}></div>
                     <div>
                         <div className="book book1" onClick={() => window.location.href = "/book"}></div>
                     </div>
@@ -103,44 +167,7 @@ function content() {
                     <div className="forStyle"></div>
                 </div>
                 <div className="books" id='scrollable'>
-                    <div className='scroll_btn' onClick={scrollPage}></div>
-                    <div>
-                        <div className="book book1" onClick={() => window.location.href = "/book"}></div>
-                    </div>
-                    <div>
-                        <div className="book book2" onClick={() => window.location.href = "/book"}></div>
-                    </div>
-                    <div>
-                        <div className="book book3" onClick={() => window.location.href = "/book"}></div>
-                    </div>
-                    <div>
-                        <div className="book book4" onClick={() => window.location.href = "/book"}></div>
-                    </div>
-                    <div>
-                        <div className="book book1" onClick={() => window.location.href = "/book"}></div>
-                    </div>
-                    <div>
-                        <div className="book book2" onClick={() => window.location.href = "/book"}></div>
-                    </div>
-                    <div>
-                        <div className="book book3" onClick={() => window.location.href = "/book"}></div>
-                    </div>
-                    <div>
-                        <div className="book book4" onClick={() => window.location.href = "/book"}></div>
-                    </div>
-                </div>
-            </section>
-
-            <section className="sec sec1">
-                <div className="label1">
-                    <div className="label">
-                        <h1>Discover Our Latest Releases</h1>
-                        <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis ipsum sed felis</p>
-                    </div>
-                    <div className="forStyle"></div>
-                </div>
-                <div className="books" id='scrollable'>
-                    <div className='scroll_btn' onClick={scrollPage}></div>
+                    <div className='scroll_btn' onClick={scrollLeft}></div>
                     <div>
                         <div className="book book1" onClick={() => window.location.href = "/book"}></div>
                     </div>
