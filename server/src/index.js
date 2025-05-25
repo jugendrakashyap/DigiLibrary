07bcd8ba6c6d8d79d8036bac1eaaae1cf6052b7b
Jugendra Kashyap <jugendrakashyap@jugendrakashyap.kjugendra63@gmail.com>
2025-05-25 23:26:05 +0530

    put index file into src folder

--- server/src/index.js
+++ server/src/index.js
@@ -6,15 +6,15 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 // const helmet = require('helmet');
 
-const connectDb = require('./config/db');
-const routes = require('./routes/routes');
+const connectDb = require('../config/db');
+const routes = require('../routes/routes');
 
 const app = express();
 
 app.use(express.json());
 // app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 // app.use(helmet());
 
 connectDb();
